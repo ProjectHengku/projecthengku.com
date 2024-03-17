4f0876f4e17dbf0244d0dbe2658ef007479dbbd5
ProjectHengku <153886572+ProjectHengku@users.noreply.github.com>
2024-03-17 16:14:56 +0900

    Add fonts and stylesheets for Project Hengku website

--- projecthengkudotcom/src/routes/+page.svelte
+++ projecthengkudotcom/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="d-none">Project Hengku / 프로젝트 헹쿠</h1>
+
+<!-- responsive breakpoints: contents center when bigger than lg size -->
+<!-- header -->
+<header class="vh-100 d-flex align-items-center justify-content-start justify-content-lg-center">
+	<div class="ps-4">
+		<p class="text-title">Dream to Reality!</p>
+		<p class="text-left text-lg-center">
+			Hengku <br />
+			<span class="fs-3 fw-bold">헹쿠</span><br />
+			<strong>Project Hengku</strong> Founder <br />
+			<strong>3nLab</strong> Chief Creative Designer <br />
+		</p>
+	</div>
+</header>
+
+<!-- main -->
+<main>
+	
+</main>
