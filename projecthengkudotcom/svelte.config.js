0614aba2bba8e7663006f110b258bd83d1cd7d2b
ProjectHengku <153886572+ProjectHengku@users.noreply.github.com>
2024-05-06 18:11:01 +0900

    fixed gh-pages method

--- projecthengkudotcom/svelte.config.js
+++ projecthengkudotcom/svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,19 +11,10 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('ded') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
-export default {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
-};
+export default config;
